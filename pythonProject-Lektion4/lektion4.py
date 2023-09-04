9bc4b903cc7443de061cd94e397d2b259237a1bd
Ali Al Khafagy <Ali.Al.Khafagy94@gmail.com>
2023-09-04 10:47:53 +0200

    lektion 4

---
+++ pythonProject-Lektion4/lektion4.py
@@ -0,0 +1,6 @@
+# Konkatenering (sammanfogning) av två strängar
+str1 = "Hej"
+str2 = "världen"
+result = str1 + " " + str2
+print(str1, str2)
+# Resultat: "Hej världen"
---
+++ pythonProject-Strings/Strings.py
@@ -0,0 +1,25 @@
+
+# Konkatenering (sammanfogning) av två strängar
+"""str1 = "Hej"
+str2 = "världen"
+str3 = str1 + " " + str2 + "!"
+print(str3)
+print("Längden på strängen är: ", len(str3))
+#len räknar längden på strängen
+print("lite text o annat smått och gott.\n")
+"""
+
+#escape sekvens exempel
+#indexering
+"""str1 = "ABC"
+print("första tecknet :", str0)
+"""
+
+letters = 'ABC'
+i = 0
+for i in range(1):
+    print(letters[0])
+i += 1
+
+for letter in letters:
+    print(letter)
